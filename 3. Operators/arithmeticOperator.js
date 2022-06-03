af0662488114132da70f42d1b2f060c4f6636c05
Arafat <arafat97alim@gmail.com>
2022-06-04 01:51:32 +0530

    Arithmatic operator concept added.

---
+++ 3. Operators/arithmeticOperator.js
@@ -0,0 +1,37 @@
+console.log("Arithmatic Operator script loaded");
+
+const a = 10;
+const b = 5;
+
+let result = 0; //chanegable values
+
+//! addition
+// result = a + b;
+// console.log(result);
+
+// //! Substraction
+// result = a - b;
+// console.log(result);
+
+// //! Multiplication
+// result = a * b;
+// console.log(result);
+
+// //! Division
+// result = a / b;
+// console.log(result);
+
+// //! Modulo
+// result = a % b;
+// console.log(result);
+
+// //! Exponent
+// result = a ** b; //a power b => 2 power 3 = 2x2x2 = 6
+// console.log(result); //100000
+
+// //! Increment and decrement
+//result++; //1
+// result--; //-1
+//++result; // 1
+// --result; //-1
+console.log(result);
