52262279fe175ec61e0531200e9248b36fd79455
Arafat <arafat97alim@gmail.com>
2022-06-04 01:28:32 +0530

    statistically vs Dynamically typed concept added.

---
+++ 3. StatisticallyVsDynamically/script.js
@@ -0,0 +1,13 @@
+console.log("Script Added");
+
+//! statiscally Typed
+// name string;
+
+//Dynamically typed
+let bingo = 30;
+console.log(bingo);
+console.log(typeof bingo); // number
+
+bingo = "Bingo is the mad angles";
+console.log(bingo);
+console.log(typeof bingo); // string
