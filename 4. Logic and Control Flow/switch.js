4ee439d57f72ec9caf8e833ff02100412352cf77
Arafat <arafat97alim@gmail.com>
2022-06-04 23:26:17 +0530

    switch statement added.

---
+++ 4. Logic and Control Flow/switch.js
@@ -0,0 +1,17 @@
+let superhero = "Iron Man";
+
+switch (superhero) {
+  case "Iron Man":
+    console.log("I am Iron Man");
+    break;
+  case "Captain America":
+    console.log("I wont give up!");
+    break;
+  case "Thor":
+    console.log("Where is my Hammer?");
+    break;
+  default:
+    console.log("PLease enter valid superhero");
+}
+
+//! Iron Man
