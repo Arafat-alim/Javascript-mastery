8b50c2e8f46d1f449f54f69af29432b4b3fd16b2
Arafat <arafat97alim@gmail.com>
2022-06-04 22:33:35 +0530

    if else statement concept added.

---
+++ 4. Logic and Control Flow/if-else.js
@@ -0,0 +1,11 @@
+console.log("if-else.js");
+//! Age meter
+
+let ageing = 19;
+if (ageing > 18) {
+  console.log("You May Enter now");
+} else if (ageing === 18) {
+  console.log("You just 18 yrs, Welcome");
+} else {
+  console.log("Grow Up!");
+}
