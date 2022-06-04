e80398b83c7a5287db112658b45256e5aeb469f7
Arafat <arafat97alim@gmail.com>
2022-06-04 22:25:18 +0530

    assignment operator concept added.

---
+++ 3. Operators/assgnmentOperator.js
@@ -0,0 +1,16 @@
+console.log("assgnmentOperator.js");
+
+let number1 = 5;
+
+// number1 += 5; //5
+// number1 -= 5; //0
+// number1 *= 5; //25
+number1 /= 5; //1
+
+console.log(number1);
+
+//string
+let stringVal = "Hello";
+
+stringVal += ", I am Aquib";
+console.log(stringVal); // hello, I am Aquib
