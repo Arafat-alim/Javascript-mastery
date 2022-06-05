702e10bd730c2a5a84a4f70a0510ad90c2ca1b94
Arafat <arafat97alim@gmail.com>
2022-06-05 23:10:21 +0530

    incomplete ternary operator lesson.

---
+++ 4. Logic and Control Flow/ternaryoperator.js
@@ -0,0 +1,11 @@
+console.log("Ternary Operator");
+
+//! If/Else statement
+
+if (consdition) {
+  //line of code execcute if condition is true
+} else {
+  //line of code execute if condition is false
+}
+
+//! Ternary Operator
