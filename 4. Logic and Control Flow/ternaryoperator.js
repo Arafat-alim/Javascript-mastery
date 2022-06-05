90be3954ce36389c9dbaeb9ba38aeac0c5a5428c
Arafat <arafat97alim@gmail.com>
2022-06-06 01:16:34 +0530

    Ternary operator concept added.

--- 4. Logic and Control Flow/ternaryoperator.js
+++ 4. Logic and Control Flow/ternaryoperator.js
@@ -2,10 +2,18 @@ console.log("Ternary Operator");
 
 //! If/Else statement
 
+/*
 if (consdition) {
   //line of code execcute if condition is true
 } else {
   //line of code execute if condition is false
 }
+*/
 
 //! Ternary Operator
+/*
+condition ? true : false;
+*/
+
+let a = 18;
+a >= 18 ? console.log("Welcome") : console.log("PLease come later!");
