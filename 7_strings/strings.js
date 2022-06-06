3e3ce4b7b17212bf5dc23be7a2e9a6386c06d8aa
Arafat <arafat97alim@gmail.com>
2022-06-06 22:55:26 +0530

    basic of strings

---
+++ 7_strings/strings.js
@@ -0,0 +1,13 @@
+console.log("Strings Lesson");
+let singleQuotes = "THis is string";
+let doubleQuotes = "THis is string";
+let backTick = `${2 + 2}`;
+
+console.log(backTick); //4
+
+const greeting = 'Hi! I\'m Jhon but ppl used to call me "JOhhny".';
+console.log(greeting);
+//here we used escape character which is "backslash"
+
+let add = (a, b) => a + b;
+console.log(`Your answer is ${add(1, 2)}`); //3
