ff68bb3ff47507948036487c10d3f5416d5a3670
Arafat <arafat97alim@gmail.com>
2022-06-06 15:24:50 +0530

    concept of loop in js added.

---
+++ 4. Logic and Control Flow/loop.js
@@ -0,0 +1,33 @@
+console.log("Loop Concept");
+
+//! While loop
+// while ([condition]) {}
+console.log("While LOOP");
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+
+//! Do while()
+//syntax
+/* do {
+  //lines of codes
+} while ([condition]);
+*/
+//exmaple
+console.log("Do while LOOP");
+let ig = 0;
+do {
+  console.log(ig);
+  ig++;
+} while (ig < 10);
+
+//! forLoop
+//syntax
+//for ([initilisation]; [condition]; [finalexpression]) {}
+//example
+console.log("For LOOP");
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
