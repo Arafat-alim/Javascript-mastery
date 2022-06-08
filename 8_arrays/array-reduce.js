527591f22fa1ec06e08037e6cb41e5a27ed9e418
Arafat <arafat97alim@gmail.com>
2022-06-08 14:45:14 +0530

    added array reduce method.

--- 8_arrays/array-reduce.js
+++ 8_arrays/array-reduce.js
@@ -1,9 +1,21 @@
 console.log("array-reduce.js");
+const groceryList = [29, 45, 65, 78, 98, 65];
 
 //! foreach loop - find the total
-const groceryList = [29, 45, 65, 78, 98, 65];
 let total = 0;
 groceryList.forEach((num, i) => {
   total += num;
 });
 console.log(total); //380
+
+//! using reduce()
+let ans = groceryList.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+
+console.log(ans); //380
+
+const numbers = [1, 2, 3, 4, 5];
+//sum of the array elemets
+let sums = numbers.reduce((total, ele) => total + ele, 0);
+console.log(sums); //15
